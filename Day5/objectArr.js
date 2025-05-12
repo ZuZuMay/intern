29006bc2562aa45e88c41ecfd8698106d9133f5d
zuzumay <may821709@gmail.com>
2025-05-12 15:52:31 +0630

    Update

---
+++ Day5/objectArr.js
@@ -0,0 +1,29 @@
+//object array
+const students = [
+    {
+        name: "stude A",
+        age: 12,
+        grade: 7
+    },
+      {
+        name: "stude B",
+        age: 12,
+        grade: 7
+    },
+      {
+        name: "stude C",
+        age: 12,
+        grade: 7
+    }
+]
+const print=students.map((stu)=>{
+     console.log(stu.name)
+     console.log(stu.age)
+     console.log(stu.grade)
+    })
+//simple array
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.map(fruit => console.log(fruit))
+
+
+
